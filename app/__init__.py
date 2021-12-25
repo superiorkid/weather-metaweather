7a0ebe2131a89bba56b321a2f5a9cb3e666af5b7
superiorkid <bayiPetani@protonmail.com>
2021-12-25 18:05:35 +0800

    update weather icon folder

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
+import os
+
+WEATHER_ICON = os.path.join('static', 'weather_img')
 
 app = Flask(__name__)
 app.config.from_object(Config)
+bootstrap = Bootstrap(app)
+app.config['UPLOAD_FOLDER'] = WEATHER_ICON
 
 from app import routes
